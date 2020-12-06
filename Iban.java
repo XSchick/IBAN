ef999d30ccfc4685ff69c7e054caa67c3579fedc
Xaver Schick <Xaver.schick@web.de>
2020-12-06 04:18:12 +0100

    First Day

---
+++ Iban.java
@@ -0,0 +1,63 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+
+public class Iban {
+    public String countryCode;
+    public int checksum;
+    public BigInteger bankCode;
+    public BigInteger bankAccountCode;
+    public BigInteger mergedNumber;
+    Scanner scanner = new Scanner(System.in);
+
+    public Iban() {
+        input();
+    }
+
+    public static void main(String[] args) {
+        new Iban();
+    }
+
+    public void input() {
+        System.out.println("Laenderkennung: ");
+        countryCode = scanner.nextLine().toUpperCase();
+        System.out.println("Bankleitzahl: ");
+        bankCode = scanner.nextBigInteger();
+        System.out.println("Kontonummer: ");
+        bankAccountCode = scanner.nextBigInteger();
+        createChecksum(bankCode, bankAccountCode);
+    }
+
+    public int createChecksum(BigInteger theBankCode, BigInteger theBankAccountCode) {
+        this.mergedNumber = mergeNumbers(theBankAccountCode, theBankCode);
+        System.out.println(mergedNumber);
+        int countryCodeNumber = convertCountryCode(countryCode);
+        System.out.println(countryCodeNumber);
+        return checksum;
+    }
+
+    public int convertCountryCode(String theCountryCode) {
+        int firstLetterCode = theCountryCode.charAt(0);
+        firstLetterCode = firstLetterCode - 64;
+        int firstletterNumber = firstLetterCode + 9;
+        int secondLetterCode = theCountryCode.charAt(1);
+        secondLetterCode = secondLetterCode - 64;
+        int secondletterNumber = secondLetterCode + 9;
+        return Integer.parseInt(Integer.toString(firstletterNumber) + Integer.toString(secondletterNumber) + "00");
+    }
+
+    public BigInteger mergeNumbers(BigInteger theBankAccountCode, BigInteger theBankCode) {
+        String StringBankCode = theBankCode.toString();
+        String StringBankAccountCode = String.format("%010d", theBankAccountCode);
+        String StringMergedNumber = StringBankCode + StringBankAccountCode;
+        System.out.println(StringMergedNumber);
+        BigInteger mergedNumber = new BigInteger(StringMergedNumber);
+        System.out.println(mergedNumber);
+        return mergedNumber;
+    }
+
+
+    public void outputIban(String theCountryCode, int theChecksum, BigInteger theBankCode, BigInteger theBankAccountCode) {
+        System.out.println("IBAN:");
+    }
+}
